02654565c8eb7264699b59b6ca4797a58f6509f1
¨Herodes¨ <herodeslugo@gmail.com>
2025-08-27 10:08:41 -0400

    chore: variants

--- components/ui/modal/variants.ts
+++ components/ui/modal/variants.ts
@@ -8,5 +8,5 @@ export const variantContainerClasses: Record<string, string> = {
 export const variantModalClasses: Record<string, string> = {
   center: "rounded-xl  min-w-[80%] shadow-lg p-5 mx-5",
   bottom: "rounded-t-2xl  w-full max-h-[80%] shadow-lg pb-10 pt-10 px-5",
-  full: "w-full h-full shadow-lg p-5",
+  full: "w-full h-full shadow-lg p-5 rounded-t rounded-t-2xl",
 };
